887046cbcd3b5847f3ecae283b07b2eb9909bd3a
mingarao <maud.ingarao@ens-lyon.fr>
2015-08-18 12:53:45 +0200

    fix #37

--- static/js/skepsis.js
+++ static/js/skepsis.js
@@ -1,6 +1,15 @@
 
 
 function skepsis(){
+  
+
+  
+  var path = location.pathname;
+  var begin = path.split('/');
+  $('a[href="/'+begin[1]+'/'+begin[2]+'"]').parent().addClass('active');
+
+  
+  
    $('.more').hide();
    $('.linkPlus').click(function() {
                 $( this ).next().toggle( "fast" );
